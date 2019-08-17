7e8b39bc855d1b5be171783a97a45e7493f25210
Sugano, Shogo <sugano-github@elder-alliance.org>
2019-08-17 14:54:36 +0900

    Add files via upload

--- libs.js
+++ libs.js
@@ -54,7 +54,7 @@ function createDateArray(arg) {
   if (arg.log["１日目の夜となりました。"] != null) {
     base_date = "１日目の夜となりました。";
   } else {
-    base_date = datearray[0];
+    base_date = ret[0];
   }
 
   return [ret, base_date];
