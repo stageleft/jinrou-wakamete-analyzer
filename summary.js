203537b53f28ad0583971e9035986b84f5ead727
stageleft <sugano-github@elder-alliance.org>
2019-09-15 22:40:42 +0900

    fix bug in closing #41

--- summary.js
+++ summary.js
@@ -65,10 +65,13 @@ function updateSummary(arg) {
     if (arg.log[d].list_voted.length == 0) {
       voted.push(["×", { comingout:"村人", enemymark:"村人" }]);
     } else {
-      voted.push([arg.log[d].list_voted.join('＆'), { comingout:"村人", enemymark:"村人" }]);
-    }
-    if (arg.log[d].list_cursed.length != 0) {
-      voted.push([arg.log[d].list_cursed.join('＆'), { comingout:"村人", enemymark:"村人" }]);
+      if (arg.log[d].list_cursed.length != 0) {
+        var v = arg.log[d].list_voted;
+        v.assign(arg.log[d].list_cursed);
+        voted.push([v.join('＆'), { comingout:"村人", enemymark:"村人" }]);
+      } else {
+        voted.push([arg.log[d].list_voted.join('＆'), { comingout:"村人", enemymark:"村人" }]);
+      }
     }
 
     if (arg.log[d].list_dnoted.length != 0) {
