1ea2f444953c1de574d31967a6e2dab30f81e1fa
stageleft <sugano-github@elder-alliance.org>
2019-10-14 22:17:14 +0900

    fix #63

--- logprovider.js
+++ logprovider.js
@@ -4,6 +4,29 @@ function updateCommentLog(arg, param) {
 // Another output    : innerHtml of <div id="comment-summary" />
   var ret = document.createElement('tbody');
 
+  function createLogTableSepalator(title) {
+    var tr_daytitle = document.createElement('tr');
+
+    var td_daytitle = document.createElement('td');
+    td_daytitle.setAttribute('colspan', '2');
+
+    var td_daytitlespan1 = document.createElement('span');
+    var td_daytitleb     = document.createElement('b');
+    var td_daytitlespan2 = document.createElement('span');
+
+    td_daytitlespan1.innerText = '◆◆◆◆◆'
+    td_daytitleb.innerText     = title
+    td_daytitlespan2.innerText = '◆◆◆◆◆'
+
+    td_daytitle.insertAdjacentElement('beforeend', td_daytitlespan1);
+    td_daytitle.insertAdjacentElement('beforeend', td_daytitleb);
+    td_daytitle.insertAdjacentElement('beforeend', td_daytitlespan2);
+
+    tr_daytitle.insertAdjacentElement('beforeend', td_daytitle);
+
+    return tr_daytitle;
+  }
+
   function createLogTableRow(l) {
     // 以下のログの形式（コピペ後）を再現できるように。
     // <tr><td valign="top" width="160"><font color="#ff9999">◆</font><b>五十嵐響子</b>さん</td><td>「えへへっ　PCS！いぇーい♪」</td></tr>
@@ -42,63 +65,42 @@ function updateCommentLog(arg, param) {
   var date_count = datearray.length;
 
   if (param.indexOf('date-log-') == 0) {
-    // create log by date
+    // create day log by date
     var datestr = datearray[Number(param.replace('date-log-','')) - 1];
-
-    var tr_daytitle = document.createElement('tr');
-
-    var td_daytitle = document.createElement('td');
-    td_daytitle.setAttribute('colspan', '2');
-
-    var td_daytitlespan1 = document.createElement('span');
-    var td_daytitleb     = document.createElement('b');
-    var td_daytitlespan2 = document.createElement('span');
-
-    td_daytitlespan1.innerText = '◆◆◆◆◆'
-    td_daytitleb.innerText     = datestr
-    td_daytitlespan2.innerText = '◆◆◆◆◆'
-
-    td_daytitle.insertAdjacentElement('beforeend', td_daytitlespan1);
-    td_daytitle.insertAdjacentElement('beforeend', td_daytitleb);
-    td_daytitle.insertAdjacentElement('beforeend', td_daytitlespan2);
-
-    tr_daytitle.insertAdjacentElement('beforeend', td_daytitle);
-
-    ret.insertAdjacentElement('beforeend', tr_daytitle);
-
+    ret.insertAdjacentElement('beforeend', createLogTableSepalator(datestr));
     arg.log[datestr].comments.forEach(function(l){
       ret.insertAdjacentElement('beforeend', createLogTableRow(l));
     });
+    // create previous night log by date (if day2..N)
+    var datestr_night = logTag_d2n(datestr);
+    if (datestr_night != datestr) {
+      ret.insertAdjacentElement('beforeend', createLogTableSepalator(datestr_night));
+      arg.log[datestr_night].comments.forEach(function(l){
+        ret.insertAdjacentElement('beforeend', createLogTableRow(l));
+      });
+    }
   } else {
     var villager_str = param.replace('all-day-log-','');
     // create log by villager
     for (var i = date_count ; i >= 1 ; i-- ) {
-      var tr_daytitle = document.createElement('tr');
-
-      var td_daytitle = document.createElement('td');
-      td_daytitle.setAttribute('colspan', '2');
-
-      var td_daytitlespan1 = document.createElement('span');
-      var td_daytitleb     = document.createElement('b');
-      var td_daytitlespan2 = document.createElement('span');
-
-      td_daytitlespan1.innerText = '◆◆◆◆◆'
-      td_daytitleb.innerText     = datearray[i-1]
-      td_daytitlespan2.innerText = '◆◆◆◆◆'
-
-      td_daytitle.insertAdjacentElement('beforeend', td_daytitlespan1);
-      td_daytitle.insertAdjacentElement('beforeend', td_daytitleb);
-      td_daytitle.insertAdjacentElement('beforeend', td_daytitlespan2);
-
-      tr_daytitle.insertAdjacentElement('beforeend', td_daytitle);
-
-      ret.insertAdjacentElement('beforeend', tr_daytitle);
-
-      arg.log[datearray[i-1]].comments.forEach(function(l){
-        if ( l.speaker.trim() == villager_str) {
+      var datestr = datearray[i-1];
+      // create day log by date
+      ret.insertAdjacentElement('beforeend', createLogTableSepalator(datestr));
+      arg.log[datestr].comments.forEach(function(l){
+        if (l.speaker.trim() == villager_str) {
           ret.insertAdjacentElement('beforeend', createLogTableRow(l));
         }
       });
+      // create previous night log by date (if day2..N)
+      var datestr_night = logTag_d2n(datestr);
+      if (datestr_night != datestr) {
+        ret.insertAdjacentElement('beforeend', createLogTableSepalator(datestr_night));
+        arg.log[datestr_night].comments.forEach(function(l){
+          if (l.speaker.trim() == villager_str) {
+            ret.insertAdjacentElement('beforeend', createLogTableRow(l));
+          }
+        });
+      }
     }
   }
 
