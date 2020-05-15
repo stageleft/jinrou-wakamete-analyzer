d1b7fd254c6b574b59a26a688a4bc915a0bdc812
stageleft <sugano-github@elder-alliance.org>
2020-05-16 00:42:56 +0900

    remove unused element of send_object

--- src/wakamete-plugins.js
+++ src/wakamete-plugins.js
@@ -27,10 +27,8 @@ function handleError(error){
 function onLogLoad(event) {
   var target = document.getElementsByTagName("form").item(0);
 
-  var village_log_txtC = JSON.parse(JSON.stringify(target.textContent));
-  var village_log_text = JSON.parse(JSON.stringify(target.innerText));
   var village_log_html = JSON.parse(JSON.stringify(target.innerHTML));
-  var village_msg = { html_log: village_log_html, text_log: village_log_text, txtc_log: village_log_txtC};
+  var village_msg = { html_log: village_log_html};
   var send_object = browser.runtime.sendMessage(village_msg);
   send_object.then(handleResponse, handleError);
 }
