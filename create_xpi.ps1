e2facc447cec8a9a3fe40d4059768e82ea605163
stageleft <sugano-github@elder-alliance.org>
2021-07-20 21:34:40 +0900

    change url jonrou.dip.jp -> 124.32.233.3

--- create_xpi.ps1
+++ create_xpi.ps1
@@ -1,7 +1,11 @@
 # Powershell script to create jinrou-wakamete-analyzer.xpi
 
-Remove-Item 'jinrou-wakamete-analyzer.xpi'
-Remove-Item  'jinrou-wakamete-analyzer.zip'
+if (Test-Path 'jinrou-wakamete-analyzer.xpi' ){
+    Remove-Item 'jinrou-wakamete-analyzer.xpi'
+}
+if (Test-Path 'jinrou-wakamete-analyzer.zip' ){
+    Remove-Item  'jinrou-wakamete-analyzer.zip'
+}
 
 Compress-Archive -Path 'src\*' -DestinationPath 'jinrou-wakamete-analyzer.zip'
 
