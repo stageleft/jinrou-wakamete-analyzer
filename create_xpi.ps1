ff391f00e0c234ee3ef0dd9620897b53ed26f2c0
stageleft <sugano-github@elder-alliance.org>
2021-07-26 20:21:59 +0900

    アイコンを認識できない。 Fixes #133

--- create_xpi.ps1
+++ create_xpi.ps1
@@ -4,7 +4,7 @@ if (Test-Path 'jinrou-wakamete-analyzer.xpi' ){
     Remove-Item 'jinrou-wakamete-analyzer.xpi'
 }
 if (Test-Path 'jinrou-wakamete-analyzer.zip' ){
-    Remove-Item  'jinrou-wakamete-analyzer.zip'
+    Remove-Item 'jinrou-wakamete-analyzer.zip'
 }
 
 Compress-Archive -Path 'src\*' -DestinationPath 'jinrou-wakamete-analyzer.zip'
