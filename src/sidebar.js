e880a757afe8873e4aa0a370421bc2fa70f6254c
stageleft <sugano-github@elder-alliance.org>
2021-12-27 00:16:18 +0900

    コンソールに性能劣化を表示

--- src/sidebar.js
+++ src/sidebar.js
@@ -1,6 +1,12 @@
 // ref. https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/runtime/sendMessage
 function recvLog(request, sender, sendResponse) {
-  return recvLog_proc(request, sender, sendResponse);
+  var time_start = performance.now();
+  var ret = recvLog_proc(request, sender, sendResponse);
+  var time_end   = performance.now();
+  if (time_end - time_start > 300) {
+    console.log("Process delayed. " + (time_end - time_start) + "[ms] is over 300[ms] as data send interval.");
+  }
+  return ret;
 };
 
 // 性能チューニング：コールバック関数を追加はコードの最後の方で。
