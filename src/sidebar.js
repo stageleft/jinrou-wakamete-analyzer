6276e82ba3134ce0035c59eccabdf7e1ea666e45
stageleft <sugano-github@elder-alliance.org>
2021-12-29 08:52:42 +0900

    性能劣化時に更新をスキップする機能を追加

--- src/sidebar.js
+++ src/sidebar.js
@@ -1,10 +1,25 @@
 // ref. https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/runtime/sendMessage
+const recv_cycle = 300
+var skip_count = 0
+
 function recvLog(request, sender, sendResponse) {
+  var ret;
+
   var time_start = performance.now();
-  var ret = recvLog_proc(request, sender, sendResponse);
+  if (skip_count <= 0) {
+    ret = recvLog_proc(request, sender, sendResponse);
+  } else {
+    console.log("Log update skipped, current skip_count=" + skip_count + ". ");
+    skip_count = skip_count - 1;
+    sendResponse({response: "OK"});
+    ret = undefined;
+  }
   var time_end   = performance.now();
-  if (time_end - time_start > 300) {
-    console.log("Process delayed. " + (time_end - time_start) + "[ms] is over 300[ms] as data send interval.");
+
+  var time_elapsed = time_end - time_start;
+  if (time_elapsed > recv_cycle) {
+    skip_count = parseInt((time_elapsed + 1) / recv_cycle);
+    console.log("Process delayed, set skip_count=" + skip_count + ". " + time_elapsed + "[ms] is over data receive cycle " + recv_cycle + "[ms].");
   }
   return ret;
 };
